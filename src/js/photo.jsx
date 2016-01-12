eca662809de8a24777959b567b75d0c4ae0d98b7
Rafael Felini <felini@felini.local>
2016-01-12 00:27:43 -0200

    improved photo declaration

--- src/js/photo.jsx
+++ src/js/photo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-class Photo extends React.Component {
+export default class Photo extends React.Component {
+
   render() {
     return <img alt={this.props.caption} src={this.props.src} />;
   }
+  
 }
-
-export default Photo
